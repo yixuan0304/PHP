b9fb586401cae57f644a18832c9a90c1b21c98da
yixuan0304 <125662816+yixuan0304@users.noreply.github.com>
2024-03-03 11:22:37 +0800

    Add files via upload

---
+++ 0222_hw/A1113323_0222_hw.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+</head>
+<body>
+    <?php
+        echo "Hello PHP!!!";
+        echo "<br/>";
+
+        $a=2;//$:變數
+        $b=1;
+
+        if($a>$b){
+        echo "a>b";
+    }
+
+    ?>
+</html>
