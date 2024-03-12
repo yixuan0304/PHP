06c403eea9744b9b7353aa1bd7a3d583d4439684
yixuan0304 <125662816+yixuan0304@users.noreply.github.com>
2024-03-12 18:19:51 +0800

    Create A1113323_0307_hw.php

---
+++ 0307_hw/A1113323_0307_hw.php
@@ -0,0 +1,54 @@
+<html>
+    <head>
+        <meta charset ="utf-8">
+        <title>
+        A1113323履歷表
+        </title>
+    </head>
+
+    <body>
+        <center>
+        <h1>邱怡瑄</h1>
+        <hr width="40%" color="black">
+        <img src="guitar.jpg" width="20%">
+        </center>
+        <h2>家鄉</h2>
+            <font size="4">空氣甜甜的台南</font>
+        <h2>求學經歷</h2>
+            <table border="1">
+                <tr><th>時期</th><th>學校</th></tr>
+                <tr><td>國小</td><td>佳里國小</td></tr>
+                <tr><td>國中</td><td>佳里國中</td></tr>
+                <tr><td>高中</td><td>港明高中</td></tr>
+                <tr><td>大學</td><td>高雄大學</td></tr>
+            </table>
+        <h2>興趣</h2>
+            <ol>
+                <li>聽音樂
+                <li>彈吉他
+            </ol>
+        <h2>經歷</h2>
+            <ul>
+                <li>一生弦命 伍代吉他營器材
+                <li>一生弦命 陸代吉他營器材長
+                <li>一生弦命 柒代吉他營副召、器材長、總務長
+                <li>一生弦命 捌代吉他營器材長
+            </ul>
+            <a href="伍.jpg">
+            <img src="伍.jpg" height="200">
+            </a>
+            <a href="陸.jpg">
+            <img src="陸.jpg" height="200">
+            </a>
+            <a href="柒.jpg">
+            <img src="柒.jpg" height="200">
+            </a>
+            <a href="捌.jpg">
+            <img src="捌.jpg" height="200">
+            </a>
+        <h2>聯絡方式</h2>
+        &nbsp&nbsp<a href="https://www.facebook.com/profile.php?id=100007448373737" target="new"><img src="fb.jpg" width="2%"></a><br/>
+        <a href="https://www.instagram.com/yqiu_0304/" target="new"><img src="ig.jpg" width="3%"></a>
+
+    </body>
+</html>
