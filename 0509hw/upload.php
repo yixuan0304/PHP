522d0c32ac339579d11fbbff44441375aa916c28
yixuan0304 <125662816+yixuan0304@users.noreply.github.com>
2024-05-13 14:06:58 +0800

    Delete 0509hw directory

--- 0509hw/upload.php
+++
@@ -1,9 +0,0 @@
-<meta charset = "utf-8">
-
-<form action = "uploadcheck.php" method = "post" enctype="multipart/form-data">
-
-    請選擇檔案:<input type = "file" name = "myFile"><br/>
-
-    <input type = "submit">
-    
-</form>
